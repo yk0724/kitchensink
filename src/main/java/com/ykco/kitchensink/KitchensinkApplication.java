a45d1d74255907c3879710f78590c651ebb6be99
yk0724 <yk0724@gmail.com>
2019-08-26 19:36:30 -0400

    Initial commit

---
+++ src/main/java/com/ykco/kitchensink/KitchensinkApplication.java
@@ -0,0 +1,13 @@
+package com.ykco.kitchensink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchensinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchensinkApplication.class, args);
+    }
+
+}
