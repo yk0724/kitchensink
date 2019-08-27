95640a402ecd59d0426d534123682244eb0dde35
yk0724 <yk0724@gmail.com>
2019-08-26 23:09:53 -0400

    test

--- src/main/java/com/ykco/kitchensink/KitchensinkApplication.java
+++ src/main/java/com/ykco/kitchensink/KitchensinkApplication.java
@@ -8,6 +8,7 @@ public class KitchensinkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KitchensinkApplication.class, args);
+        System.out.println("App running.");
     }
 
 }
